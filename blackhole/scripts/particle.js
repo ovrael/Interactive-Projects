0c2495e9ace2d51b4a4297bee38deadbaa2f4ce3
ovrael <ovrael@gmail.com>
2023-01-14 10:10:36 +0100

    Blackhole project updated

--- blackhole/scripts/particle.js
+++ blackhole/scripts/particle.js
@@ -1,7 +1,15 @@
 class Particle {
-    constructor(mass) {
+    constructor(mass, xPos, yPos) {
+        this.pulled = false;
+
         this.mass = mass;
-        this.position = createVector(ProjectData.CanvasWidth - 50, ProjectData.CanvasHeight / 2 - 50);
+        this.xPos = xPos;
+        this.yPos = yPos;
+
+        this.tail = [];
+        this.tailLenght = 5;
+
+        this.position = createVector(this.xPos, this.yPos);
         this.acceleration = createVector();
         this.velocity = createVector(-5, 0);
     }
@@ -9,22 +17,36 @@ class Particle {
     draw() {
         push();
 
-        strokeWeight(10);
+        strokeWeight(ProjectData.ParticleSize);
         stroke(200, 100, 50);
 
         point(this.position.x, this.position.y);
 
+        this.drawTail();
         pop();
+    }
+
+    drawTail() {
+        strokeWeight(1);
+        noFill();
+        beginShape();
+        for (let i = 0; i < this.tail.length; i++) {
+            vertex(this.tail[i].x, this.tail[i].y);
+        }
+        endShape();
     }
 
     #checkBoundaries() {
         if (this.position.x < 0 || this.position.x > ProjectData.CanvasWidth) {
-            this.position = createVector(ProjectData.CanvasWidth - 50, ProjectData.CanvasHeight / 2 - 50);
-            this.velocity = createVector();
+            // this.position = createVector(ProjectData.CanvasWidth - 50, ProjectData.CanvasHeight / 2 - 50);
+            this.velocity = createVector(-5, 0);
         }
     }
 
     update() {
+        if (this.pulled)
+            return;
+
         this.#checkBoundaries();
 
         // this.velocity.add(this.acceleration);
@@ -34,6 +56,10 @@ class Particle {
 
 
         this.acceleration.mult(0);
+        this.tail.push(this.position.copy());
+        if (this.tail.length > this.tailLength) {
+            this.tail.splice(0, 1);
+        }
     }
 
     applyForce(force) {
