3e0fe06950284c8bcbd358efda4de7d32d364928
ovrael <ovrael@gmail.com>
2023-03-04 16:49:10 +0100

    Backup before creating optimizer class.

--- shared/scripts/neuralNetwork_backup/dataPoint.js
+++ shared/scripts/neuralNetwork_backup/dataPoint.js
@@ -2,14 +2,15 @@ class DataPoint {
     constructor(inputs, label, numLabels = -1) {
         this.inputs = inputs;
         this.label = label;
+        this.expectedOutputs = label;
         if (numLabels > 0)
             this.expectedOutputs = DataPoint.createOneHot(label, numLabels);
     }
 
-    static createOneHot(index, num) {
+    static createOneHot(label, classesCount) {
         let oneHot = [];
-        for (let i = 0; i < num; i++) {
-            oneHot[index] = (i == num) ? 1 : 0;
+        for (let i = 0; i < classesCount; i++) {
+            oneHot[i] = (i == label) ? 1 : 0;
         }
         return oneHot;
     }
