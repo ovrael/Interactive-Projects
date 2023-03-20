54dc10c11f650ecbe312ce7e44ccbc8d4c1a5dd8
ovrael <ovrael@gmail.com>
2023-03-20 13:40:14 +0100

    Updated p5 template project

--- _p5Template/scripts/projectData.js
+++ _p5Template/scripts/projectData.js
@@ -1,4 +1,9 @@
 class ProjectData {
+    static SettingsName = 'P5 Template project';
+
+    // tab: Canvas
+    // section: Size
+
     static CanvasWidth = 600;               // slider,   Width,  200, 1000, 600, 10
     static CanvasHeight = 600;              // slider,   Height,  200, 1000, 600, 10
     static BackgroundColor = '#646464';     // color,    Background,  #646464
--- digitRecognizer/scripts/projectData.js
+++ digitRecognizer/scripts/projectData.js
@@ -3,7 +3,7 @@ class ProjectData {
     static CanvasHeight = 600;              // slider,   Height,  200, 1000, 600, 10
     static BackgroundColor = '#646464';     // color,    Background,  #646464
 
-    static DrawDigitStroke = 10;
+    static DrawDigitStroke = 10;            // slider,   Draw thickness,  200, 1000, 600, 10
 }
 
 // const scriptsTest = [
